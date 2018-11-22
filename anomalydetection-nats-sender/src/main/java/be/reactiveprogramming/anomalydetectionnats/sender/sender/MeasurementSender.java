aa99bc2083aa8d28ff098e36c3d4e36dbd372492
ICONOS\vande18 <steffen.vandegaer@faros.be>
2018-11-22 16:02:45 +0100

    improved UI

--- anomalydetection-nats-sender/src/main/java/be/reactiveprogramming/anomalydetectionnats/sender/sender/MeasurementSender.java
+++ anomalydetection-nats-sender/src/main/java/be/reactiveprogramming/anomalydetectionnats/sender/sender/MeasurementSender.java
@@ -22,8 +22,6 @@ import reactor.core.publisher.Mono;
 @Service
 public class MeasurementSender {
 
-  private WebClient wc;
-
   private final Random r = new Random();
 
   /**
@@ -40,18 +38,16 @@ public class MeasurementSender {
    *
    */
   public Flux<Integer> sendMeasurement(String destination, int amount) {
-    this.wc = WebClient.create("http://"+destination+":8081");
-    return Flux.range(1, amount).flatMap(number -> sendMeasurementForNumber(number));
+      WebClient wc = WebClient.create("http://"+destination+":8081");
+    return Flux.range(1, amount).flatMap(number -> {
+        final MeasurementEvent measurement = new MeasurementEvent(LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME), "" + r.nextInt(10), BigDecimal.valueOf(r.nextDouble() * 100));
+
+        return wc.method(HttpMethod.POST).uri("measurements")
+                .body(BodyInserters.fromPublisher(Mono.just(measurement), MeasurementEvent.class))
+                .exchange().map(r -> number);
+    });
   }
 
-  private Mono<Integer> sendMeasurementForNumber(int number) {
-
-    final MeasurementEvent measurement = new MeasurementEvent(LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME), "" + r.nextInt(10), BigDecimal.valueOf(r.nextDouble() * 100));
-
-    return wc.method(HttpMethod.POST).uri("measurements")
-            .body(BodyInserters.fromPublisher(Mono.just(measurement), MeasurementEvent.class))
-            .exchange().map(r -> number);
-  }
 
 
 }
--- anomalydetection-nats-sender/src/main/resources/static/measurementsender.js
+++ anomalydetection-nats-sender/src/main/resources/static/measurementsender.js
@@ -5,24 +5,54 @@ Rx.DOM.click(startSendingButton)
 var subscription;
 var numberSent = 0;
 
+document.getElementById("startSending").hidden=true;
+document.getElementById("errorMessage").innerHTML="Fill in your IP";
+
 function startSending() {
     if (subscription != null) {
         subscription.dispose();
     }
     document.getElementById("message").innerHTML = "";
 
-    var eventSource = Rx.DOM.fromEventSource("/sendMeasurements/"+document.getElementById("gatewayIp").value+"/amount/"+document.getElementById("numberOfMessages").value);
+        var eventSource = Rx.DOM.fromEventSource("/sendMeasurements/"+document.getElementById("gatewayIp").value+"/amount/"+document.getElementById("numberOfMessages").value);
 
-    eventSource.doOnError();
+            eventSource.doOnError();
 
-    subscription = eventSource
-        .subscribe(
-            function (n) { return addToLog()},
-            function(e) {}
-        );
+            subscription = eventSource
+                .subscribe(
+                    function (n) { return addToLog()},
+                    function(e) {}
+                );
+
+
 }
 
 function addToLog() {
     numberSent++;
     document.getElementById("message").innerHTML = "Total number sent: " + numberSent;
+}
+
+function changeNumberOfMessages(){
+    document.getElementById("startSending").hidden=false;
+    document.getElementById("errorMessage").innerHTML="";
+
+    if((document.getElementById("numberOfMessages").value<=0)||(document.getElementById("numberOfMessages").value>50000)){
+        document.getElementById("startSending").hidden=true;
+        document.getElementById("errorMessage").innerHTML="Pick a number between 0 and 50000 and fill in your IP";
+    }
+    if(document.getElementById("gatewayIp").value==""){
+        document.getElementById("startSending").hidden=true;
+        document.getElementById("errorMessage").innerHTML="Fill in your IP";
+    }
+    if(ValidateIPaddress(document.getElementById("gatewayIp").value)){
+        document.getElementById("startSending").hidden=true;
+        document.getElementById("errorMessage").innerHTML="invalid IP";
+    }
+}
+
+function ValidateIPaddress(ipaddress) {
+  if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ipaddress)) {
+    return (false)
+  }
+  return (true)
 }
